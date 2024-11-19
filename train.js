8963126d5482d086bec71012e2c42ca68de759bd
aziz1407 <aibrokhimov2003@gmail.com>
2024-11-19 21:42:50 +0900

    feat: A-task!

--- train.js
+++ train.js
@@ -23,9 +23,9 @@ function countO (o, pneumonoultramicroscopicsilicovolcanoconiosis) {
 // Maqsad: parametr 1 parametr 2'da necha marta ishlatilganligini return qilish.
 // Step-2: let variablelini hosil qilib uning qiymatini 0ga tengladim, chunki shunda sanoq noldan boshlanadi!
 // Step-3: for loopini ochib oldim, chunki looplar parameterimni har harfini dona-dona tekshirib, true yani bu case'da (o) ni izlaydi.
-// Step-4: loopni ichida variable ochib uni qiymatini 0ga tengladim, va bu a'ning qiymati ya'ni 0, parametr 2'ni loop qilishni 0 indexdan
+// Step-4: loopni ichida variable ochib uni qiymatini 0ga tengladim, va bu a'ning qiymati ya'ni 0, parametr 2'ni loop qilishni 0-indexdan
 // boshlaydi degani.
-// Step-5: Loop false holatiga yetib borguncha ishga tushadi yani parametr 2'da nechta harf bolsa shuncha marta ishga tushib, to'xtaydi.
+// Step-5: Loop false holatiga yetib borguncha ishga tushadi, ya'ni parametr 2'da nechta harf bolsa shuncha marta ishga tushib, to'xtaydi.
 // Step-6: condition holatida: parameter ikkida 0'chi indexdan boshlangan qiymat (o)ga teng bo'lsa condition true bo'ladi va count++ 
 // true qiymatlarni qo'shib borishni ta'minlaydi.
 // Last step: function call qilindi!
