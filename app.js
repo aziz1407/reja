86d80062e412e7bafe3b308788ea6db11139dddb
aziz1407 <aibrokhimov2003@gmail.com>
2024-11-22 10:54:12 +0900

    feat: MongoDB connection

---
+++ app.js
@@ -0,0 +1,46 @@
+console.log('Running web server!');
+const express = require('express');
+const app = express();
+
+const fs = require("fs");
+
+// MongoDB call
+const db = require('./server').db();
+
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+})
+
+// 1: Entry codes
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2: Session related codes
+// 3: Related to Views
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+
+// 4: Related to routing
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"});
+});
+
+app.get("/author", (req, res) => {
+    res.render("author", {user: user})
+});
+
+app.get('/', function (req, res) {
+    res.render("reja");
+});
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,45 +1,25 @@
-console.log('Running web server!');
-const express = require('express');
-const app = express();
 const http = require('http');
-const fs = require("fs");
+const mongodb = require('mongodb');
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data)
+let db;
+const connectionString =
+"mongodb+srv://Aiden:Aiden14@cluster0.xd5lx.mongodb.net/Reja"
+
+mongodb.connect(connectionString, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}, (err, client) => {
+    if(err) console.log('MongoDB connection failed!')
+        else {
+            console.log('MongoDB connection successful!');
+            module.exports = client;
+
+            const app = require('./app');
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function() {
+            console.log(`Everything working pretty smoothly: ${PORT}, http://localhost:3000`);
+});
     }
-})
-
-// 1: Entry codes
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// 2: Session related codes
-// 3: Related to Views
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-
-// 4: Related to routing
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"});
 });
 
-app.get("/author", (req, res) => {
-    res.render("author", {user: user})
-});
-
-app.get('/', function (req, res) {
-    res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function() {
-   console.log(`Everything working pretty smoothly: ${PORT}, http://localhost:3000`);
-});
